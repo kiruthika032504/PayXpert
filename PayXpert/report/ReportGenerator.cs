26787187ba0ef4c8a01298ac6a99b6ca6a2f33d8
Kiruthika Vijay <168557782+kiruthika032504@users.noreply.github.com>
2024-05-21 00:48:59 +0530

    Update ReportGenerator.cs

--- PayXpert/report/ReportGenerator.cs
+++ PayXpert/report/ReportGenerator.cs
@@ -1,14 +1,47 @@
-﻿using PayXpert.model;
+using PayXpert.model;
+using PayXpert.util;
+using System.Data.SqlClient;
 
 namespace PayXpert.report
 {
     public class ReportGenerator
     {
+        private SqlConnection sqlConnection = null;
+        private SqlCommand cmd = null;
+
+        public ReportGenerator()
+        {
+            sqlConnection = new SqlConnection(DbConnUtil.GetConnectionString());
+            cmd = new SqlCommand();
+            cmd.Connection = sqlConnection;
+        }
         // Generate various reports based on data
 
         // Example method to generate a payroll report
         public void GeneratePayrollReport(List<Payroll> payrolls)
         {
+            cmd.CommandText = "SELECT * FROM Payroll";
+            sqlConnection.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Payroll payrollData = new Payroll()
+                {
+                    PayrollId = Convert.ToInt32(reader["PayrollId"]),
+                    EmployeeId = Convert.ToInt32(reader["EmployeeId"]),
+                    PayPeriodStartDate = Convert.ToDateTime(reader["PayPeriodStartDate"]),
+                    PayPeriodEndDate = Convert.ToDateTime(reader["PayPeriodEndDate"]),
+                    BasicSalary = Convert.ToDecimal(reader["BasicSalary"]),
+                    OvertimePay = Convert.ToDecimal(reader["OvertimePay"]),
+                    OvertimeHours = Convert.ToInt32(reader["OvertimeHours"]),
+                    DA = Convert.ToDecimal(reader["DA"]),
+                    RA = Convert.ToDecimal(reader["RA"]),
+                    Deductions = Convert.ToDecimal(reader["Deductions"]),
+                    GrossSalary = Convert.ToDecimal(reader["GrossSalary"]),
+                    NetSalary = Convert.ToDecimal(reader["NetSalary"])
+                };
+                payrolls.Add(payrollData);
+            }
             string reportContent = "Payroll Report:\n\n";
             foreach (var payroll in payrolls)
             {
@@ -18,16 +51,36 @@ namespace PayXpert.report
                 reportContent += $"Period End Date: {payroll.PayPeriodEndDate}\n";
                 reportContent += $"Basic Salary: {payroll.BasicSalary}\n";
                 reportContent += $"Overtime Pay: {payroll.OvertimePay}\n";
+                reportContent += $"Overtime Hours: {payroll.OvertimeHours}\n";
+                reportContent += $"Dearness Allowance(DA): {payroll.DA}\n";
+                reportContent += $"Risk Allowance(RA): {payroll.RA}\n";
                 reportContent += $"Deductions: {payroll.Deductions}\n";
+                reportContent += $"Gross Salary: {payroll.GrossSalary}\n";
                 reportContent += $"Net Salary: {payroll.NetSalary}\n\n";
             }
 
             GenerateReport("PayrollReport", reportContent);
+            sqlConnection.Close();
         }
 
         // Example method to generate a tax report
         public void GenerateTaxReport(List<Tax> taxes)
         {
+            cmd.CommandText = "SELECT * FROM Tax";
+            sqlConnection.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Tax tax = new Tax()
+                {
+                    TaxId = Convert.ToInt32(reader["TaxId"]),
+                    EmployeeId = Convert.ToInt32(reader["EmployeeId"]),
+                    TaxableIncome = Convert.ToDecimal(reader["TaxableIncome"]),
+                    TaxAmount = Convert.ToDecimal(reader["TaxAmount"]),
+                    TaxYear = Convert.ToInt32(reader["TaxYear"])
+                };
+                taxes.Add(tax);
+            }
             string reportContent = "Tax Report:\n\n";
             foreach (var tax in taxes)
             {
@@ -39,11 +92,28 @@ namespace PayXpert.report
             }
 
             GenerateReport("TaxReport", reportContent);
+            sqlConnection.Close();
         }
 
         // Example method to generate a financial record report
         public void GenerateFinancialRecordReport(List<FinancialRecord> records)
         {
+            cmd.CommandText = "SELECT * FROM FinancialRecord";
+            sqlConnection.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                FinancialRecord financialRecord = new FinancialRecord()
+                {
+                    RecordId = Convert.ToInt32(reader["RecordId"]),
+                    EmployeeId = Convert.ToInt32(reader["EmployeeId"]),
+                    RecordDate = Convert.ToDateTime(reader["RecordDate"]),
+                    RecordType = Convert.ToString(reader["RecordType"]),
+                    Amount = Convert.ToDecimal(reader["Amount"]),
+                    Description = Convert.ToString(reader["Description"])
+                };
+                records.Add(financialRecord);
+            }
             string reportContent = "Financial Record Report:\n\n";
             foreach (var record in records)
             {
@@ -56,6 +126,7 @@ namespace PayXpert.report
             }
 
             GenerateReport("FinancialRecordReport", reportContent);
+            sqlConnection.Close();
         }
 
         // Method to generate a generic report
